332d162c27991797f7d661981a11f1f52c0b0ad9
Richard Li <lichard49@gmail.com>
2022-03-07 00:54:00 -0800

    Compile Android in standalone release mode

--- run_android.sh
+++ run_android.sh
@@ -1,5 +1,5 @@
 #adb reverse tcp:8081 tcp:8081
-npx react-native run-android --verbose
+npx react-native run-android --verbose --variant=release
 
 # error: couldn't find DSO to load: libfbjni.so
 # https://stackoverflow.com/a/63131031
