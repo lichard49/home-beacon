5bb2c29b4022067804ffbff3e064802825fc17e6
Richard Li <lichard49@gmail.com>
2022-04-18 17:55:36 -0700

    Parameterize iOS build script to enable specifying device

--- run_ios.sh
+++ run_ios.sh
@@ -1,2 +1,12 @@
 # npx react-native run-ios
-npx react-native run-ios --device "iPhone" --configuration Release
+
+target_device="iPhone"
+
+if [ "$1" ]
+then
+  target_device=$1
+fi
+
+echo "installing app to device: $target_device"
+
+npx react-native run-ios --device "$target_device" --configuration Release
