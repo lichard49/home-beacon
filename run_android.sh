a9fd356649310c8275a6cb90ba526e01e01b94ff
Richard Li <lichard49@gmail.com>
2023-05-29 22:21:03 -0700

    Get Android to compile again

--- run_android.sh
+++ run_android.sh
@@ -1,5 +1,8 @@
 #adb reverse tcp:8081 tcp:8081
 npx react-native run-android --verbose --variant=release
 
+# error: More than one file was found with OS independent path 'lib/armeabi-v7a/libfbjni.so'.
+# https://stackoverflow.com/questions/74327301/more-than-one-file-was-found-with-os-independent-path-lib-armeabi-v7a-libfbjni
+
 # error: couldn't find DSO to load: libfbjni.so
 # https://stackoverflow.com/a/63131031
